9d7b5bc1bea7aaa6f12581be02841211be545583
fra-yer <66422169+fra-yer@users.noreply.github.com>
2026-05-02 20:56:43 +0300

    Update sensor.py
    v1.6.1

--- custom_components/earthquake_monitor/sensor.py
+++ custom_components/earthquake_monitor/sensor.py
@@ -3,6 +3,9 @@
 # Extended with improved event-selection and location-description logic
 # See accompanying README.md for details
 # Version 1.6.1 by FOF, May 2026
+# change-log:
+#   initialize new entities with status = "cleared"
+#   change offshore fallback value from "international waters" to "offshore"
 
 import asyncio
 import io
