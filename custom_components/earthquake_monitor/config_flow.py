42bbdf8e87e29571ee81d3f198d3b92bc277ae8d
fra-yer <66422169+fra-yer@users.noreply.github.com>
2026-05-05 20:03:14 +0300

    Update config_flow.py

--- custom_components/earthquake_monitor/config_flow.py
+++ custom_components/earthquake_monitor/config_flow.py
@@ -1,6 +1,6 @@
 # Version 1.7.0 by FOF, May 2026
 # change-log of 1.7.0
-#    no changes from 1.6.2
+#    no changes from 1.6.2 in this file
 
 from homeassistant import config_entries
 import voluptuous as vol
