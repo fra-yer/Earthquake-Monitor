5cb0c428afddbbe6191339e2922d47afcfb65595
Frank O. Fackelmayer <66422169+fra-yer@users.noreply.github.com>
2026-05-15 08:22:00 +0300

    Update config_flow.py

--- custom_components/earthquake_monitor/config_flow.py
+++ custom_components/earthquake_monitor/config_flow.py
@@ -1,5 +1,5 @@
-# Version 1.7.7 by FOF, May 2026
-# change-log of 1.7.7
+# Version 1.8.0 by FOF, May 2026
+# change-log of 1.8.0
 #    no changes from 1.6.2 in this file
 
 from homeassistant import config_entries
